4e3132e7028bbe0e1ee366b706d5a1f3f87ad3ac
Atharva Munshi <atharvachaitanya.munshi@sjsu.edu>
2020-10-21 20:34:54 -0700

    Created signin route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -37,4 +37,27 @@ router.post("/signup", (req, res) => {
     });
 });
 
+router.post("/signin", (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(422).json({ error: "please add email or password" });
+  }
+  User.findOne({ email: email }).then((savedUser) => {
+    if (!savedUser) {
+      return res.status(422).json({ error: "Invalid Email or Password" });
+    }
+    bcrypt
+      .compare(password, savedUser.password)
+      .then((doMatch) => {
+        if (doMatch) {
+          res.json({ message: "successfully signed in" });
+        } else {
+          return res.status(422).json({ error: "Invalid Email or Password" });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+});
 module.exports = router;
