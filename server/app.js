dad3760899af786329a381b5e00fe15fdbb1d463
Atharva Munshi <atharvachaitanya.munshi@sjsu.edu>
2020-10-24 18:00:26 -0700

    Created Post schema and post route

--- server/app.js
+++ server/app.js
@@ -4,9 +4,6 @@ const mongoose = require("mongoose");
 const PORT = 5000;
 const { MONGOURI } = require("./keys");
 
-require("./models/user");
-app.use(express.json());
-app.use(require("./routes/auth"));
 mongoose.connect(MONGOURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -17,6 +14,12 @@ mongoose.connection.on("connected", () => {
 mongoose.connection.on("error", (err) => {
   console.log("Error in connecting", err);
 });
+require("./models/user");
+require("./models/post");
+
+app.use(express.json());
+app.use(require("./routes/auth"));
+app.use(require("./routes/post"));
 
 app.listen(PORT, () => {
   console.log("server running on port", PORT);
