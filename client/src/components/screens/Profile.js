94b9086a020659550ff66e4286261041d7b1071b
Atharva Munshi <atharvachaitanya.munshi@sjsu.edu>
2020-11-08 15:06:58 -0800

    Updating follow and unfollow of logged in user

--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -41,9 +41,9 @@ const Profile = () => {
               width: "108%",
             }}
           >
-            <h6>40 Posts</h6>
-            <h6>40 Followers</h6>
-            <h6>40 Following</h6>
+            <h6>{mypics.length} posts</h6>
+            <h6>{state ? state.followers.length : "0"} followers</h6>
+            <h6>{state ? state.following.length : "0"} following</h6>
           </div>
         </div>
       </div>
